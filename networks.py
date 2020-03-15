d96054b7b28ba26f60d8b73d9bd5b00848fb6327
Jayneel Parekh <jayneelparekh@gmail.com>
2020-03-15 14:09:46 +0100

    Update networks.py
    Fixing space tab issue

--- networks.py
+++ networks.py
@@ -16,7 +16,7 @@ class res_block(nn.Module):
     def forward(self, inp):
         mid = self.conv(inp)
         mid, hidden = self.recurrent(mid.permute(0,2,1))
-	mid = mid.permute(0,2,1)
+        mid = mid.permute(0,2,1)
         #out = self.transp_conv(mid)
         out = inp + self.transp_conv(mid)
         return out
@@ -26,17 +26,17 @@ class res_block(nn.Module):
 class exp_net(nn.Module):
     def __init__(self, input_size, output_size, freq=513):
         super(exp_net, self).__init__()
-	self.down_f0 = nn.Conv1d(freq, freq-1, 3, stride=1, padding=1)
-	self.down_f1 = nn.Conv1d(freq-1, (freq-1)/2, 5, stride=1, padding=2)
+        self.down_f0 = nn.Conv1d(freq, freq-1, 3, stride=1, padding=1)
+        self.down_f1 = nn.Conv1d(freq-1, (freq-1)/2, 5, stride=1, padding=2)
         self.down_f2 = nn.Conv1d((freq-1)/2, (freq-1)/4, 5, stride=1, padding=2)
         self.down_f3 = nn.Conv1d((freq-1)/4, (freq-1)/8, 5, stride=1, padding=2)
-	self.down_t0 = nn.Conv1d(freq-1, freq-1, 5, stride=2, padding=2)
-	self.down_t1 = nn.Conv1d((freq-1)/2, (freq-1)/2, 5, stride=2, padding=2)
-	self.down_t2 = nn.Conv1d((freq-1)/4, (freq-1)/4, 5, stride=2, padding=2)
+        self.down_t0 = nn.Conv1d(freq-1, freq-1, 5, stride=2, padding=2)
+        self.down_t1 = nn.Conv1d((freq-1)/2, (freq-1)/2, 5, stride=2, padding=2)
+        self.down_t2 = nn.Conv1d((freq-1)/4, (freq-1)/4, 5, stride=2, padding=2)
 
-	self.activ = nn.LeakyReLU()
+        self.activ = nn.LeakyReLU()
 
-	self.inp_size = input_size
+        self.inp_size = input_size
         self.out_size = output_size
         self.lstm = nn.GRU((freq-1)/4, (freq-1)/4, num_layers=1, batch_first=True, dropout=0.1)
         
@@ -46,14 +46,14 @@ class exp_net(nn.Module):
         C1 = self.activ(  self.down_f0(inp)   )
         C1 = self.activ(  self.down_t0(C1)  )
         C2 = self.activ(  self.down_f1(C1)  )
-	C3 = self.activ(  self.down_t1(C2)  )
-	C4 = self.activ(  self.down_f2(C3)  )
+        C3 = self.activ(  self.down_t1(C2)  )
+        C4 = self.activ(  self.down_f2(C3)  )
 
         C4, hidden = self.lstm(C4.permute(0,2,1))
-	C4 = C4.permute(0,2,1)
+        C4 = C4.permute(0,2,1)
 
-	C5 = self.activ(  self.down_t2(C4)  )
-	output = self.activ(  self.down_f3(C5)  )
+        C5 = self.activ(  self.down_t2(C4)  )
+        output = self.activ(  self.down_f3(C5)  )
         
         return output
 
@@ -65,29 +65,29 @@ class exp_net(nn.Module):
 class net_base(nn.Module):
     def __init__(self, input_size, output_size, freq=513):
         super(net_base, self).__init__()
-	self.down_f0 = nn.Conv1d(freq, freq-1, 3, stride=1, padding=1)
-	self.down_f1 = nn.Conv1d(freq-1, (freq-1)/2, 5, stride=1, padding=2)
+        self.down_f0 = nn.Conv1d(freq, freq-1, 3, stride=1, padding=1)
+        self.down_f1 = nn.Conv1d(freq-1, (freq-1)/2, 5, stride=1, padding=2)
         self.down_f2 = nn.Conv1d((freq-1)/2, (freq-1)/4, 5, stride=1, padding=2)
         self.down_f3 = nn.Conv1d((freq-1)/4, (freq-1)/8, 5, stride=1, padding=2)
-	self.down_t0 = nn.Conv1d(freq-1, freq-1, 5, stride=2, padding=2)
-	self.down_t1 = nn.Conv1d((freq-1)/2, (freq-1)/2, 5, stride=2, padding=2)
-	self.down_t2 = nn.Conv1d((freq-1)/4, (freq-1)/4, 5, stride=2, padding=2)
+        self.down_t0 = nn.Conv1d(freq-1, freq-1, 5, stride=2, padding=2)
+        self.down_t1 = nn.Conv1d((freq-1)/2, (freq-1)/2, 5, stride=2, padding=2)
+        self.down_t2 = nn.Conv1d((freq-1)/4, (freq-1)/4, 5, stride=2, padding=2)
 
         self.up_f1 = nn.ConvTranspose1d((freq-1)/4, (freq-1)/4, 5, padding=2)
         self.up_f2 = nn.ConvTranspose1d((freq-1)/4, (freq-1)/2, 5, padding=2)
         self.up_f3 = nn.ConvTranspose1d((freq-1)/2, freq-1, 5, padding=2)
-	self.up_f4 = nn.ConvTranspose1d(freq-1, freq, 5, padding=2)
+        self.up_f4 = nn.ConvTranspose1d(freq-1, freq, 5, padding=2)
         self.up_t1 = nn.ConvTranspose1d((freq-1)/4, (freq-1)/4, 3, stride=2, padding=1, output_padding=1)
         self.up_t2 = nn.ConvTranspose1d((freq-1)/2, (freq-1)/2, 3, stride=2, padding=1, output_padding=1)
-	self.up_t3 = nn.ConvTranspose1d(freq-1, freq-1, 3, stride=2, padding=1, output_padding=1)
+        self.up_t3 = nn.ConvTranspose1d(freq-1, freq-1, 3, stride=2, padding=1, output_padding=1)
 
-	self.activ = nn.LeakyReLU()
+        self.activ = nn.LeakyReLU()
 
         self.inp_size = input_size
         self.out_size = output_size
         self.lstm_1 = nn.GRU((freq-1)/4, (freq-1)/4, num_layers=1, batch_first=True, dropout=0.3)
         self.lstm_2 = nn.GRU(freq-1, freq-1, num_layers=1, batch_first=True, dropout=0.3)
-	self.dropout = nn.Dropout(0.05)
+        self.dropout = nn.Dropout(0.05)
         self.freq = freq
 
 
@@ -98,29 +98,29 @@ class net_base(nn.Module):
         C1 = self.activ( self.down_f0(inp)  )
         C1 = self.activ( self.down_t0(C1)   )
         C2 = self.activ( self.down_f1(C1)   )
-	C3 = self.activ( self.down_t1(C2)   )
-	C4 = self.activ( self.down_f2(C3)   )
+        C3 = self.activ( self.down_t1(C2)   )
+        C4 = self.activ( self.down_f2(C3)   )
 
         C4, hidden = self.lstm_1(C4.permute(0,2,1))
-	C4 = C4.permute(0,2,1)
-	C5 = self.activ(  self.down_t2(C4)  )
-	encoding1 = self.activ(  self.down_f3(C5)  )
+        C4 = C4.permute(0,2,1)
+        C5 = self.activ(  self.down_t2(C4)  )
+        encoding1 = self.activ(  self.down_f3(C5)  )
 
         encoding = torch.cat((encoding1, pitch_code), 1)
 
         # Decode
-	C6 = self.activ(  self.up_f1(encoding)  )
-	C5 = self.activ(  self.up_t1(C6)    )
-	C4 = self.activ(  self.up_f2(C5)  )
-	C3 = self.activ(  self.up_t2(C4)  )
-	C2 = self.activ(  self.up_f3(C3)  )
-	output = self.activ(  self.up_t3(C2)  )
+        C6 = self.activ(  self.up_f1(encoding)  )
+        C5 = self.activ(  self.up_t1(C6)    )
+        C4 = self.activ(  self.up_f2(C5)  )
+        C3 = self.activ(  self.up_t2(C4)  )
+        C2 = self.activ(  self.up_f3(C3)  )
+        output = self.activ(  self.up_t3(C2)  )
 
-	#output = self.activ(  self.up_f4(output)   )
+        #output = self.activ(  self.up_f4(output)   )
 
-	output, hidden = self.lstm_2(output.permute(0,2,1))
+        output, hidden = self.lstm_2(output.permute(0,2,1))
 
-	output = self.activ(  self.up_f4(output.permute(0,2,1))   )
+        output = self.activ(  self.up_f4(output.permute(0,2,1))   )
         
         #return output
         return output, encoding1
@@ -134,36 +134,36 @@ class net_base(nn.Module):
 class net_in_v2(nn.Module):
     def __init__(self, input_size, output_size, freq=513):
         super(net_in_v2, self).__init__()
-	self.down_f0 = nn.Conv1d(freq, freq-1, 3, stride=1, padding=1)
-	self.down_f1 = nn.Conv1d(freq-1, (freq-1)/2, 5, stride=1, padding=2)
+        self.down_f0 = nn.Conv1d(freq, freq-1, 3, stride=1, padding=1)
+        self.down_f1 = nn.Conv1d(freq-1, (freq-1)/2, 5, stride=1, padding=2)
         self.down_f2 = nn.Conv1d((freq-1)/2, (freq-1)/4, 5, stride=1, padding=2)
         self.down_f3 = nn.Conv1d((freq-1)/4, (freq-1)/8, 5, stride=1, padding=2)
-	self.down_t0 = nn.Conv1d(freq-1, freq-1, 5, stride=2, padding=2)
-	self.down_t1 = nn.Conv1d((freq-1)/2, (freq-1)/2, 5, stride=2, padding=2)
-	self.down_t2 = nn.Conv1d((freq-1)/4, (freq-1)/4, 5, stride=2, padding=2)
+        self.down_t0 = nn.Conv1d(freq-1, freq-1, 5, stride=2, padding=2)
+        self.down_t1 = nn.Conv1d((freq-1)/2, (freq-1)/2, 5, stride=2, padding=2)
+        self.down_t2 = nn.Conv1d((freq-1)/4, (freq-1)/4, 5, stride=2, padding=2)
 
         self.up_f1 = nn.ConvTranspose1d((freq-1)/4, (freq-1)/4, 5, padding=2)
         self.up_f2 = nn.ConvTranspose1d((freq-1)/4, (freq-1)/2, 5, padding=2)
         self.up_f3 = nn.ConvTranspose1d((freq-1)/2, freq-1, 5, padding=2)
-	self.up_f4 = nn.ConvTranspose1d(freq-1, freq, 5, padding=2)
+        self.up_f4 = nn.ConvTranspose1d(freq-1, freq, 5, padding=2)
         self.up_t1 = nn.ConvTranspose1d((freq-1)/4, (freq-1)/4, 3, stride=2, padding=1, output_padding=1)
         self.up_t2 = nn.ConvTranspose1d((freq-1)/2, (freq-1)/2, 3, stride=2, padding=1, output_padding=1)
-	self.up_t3 = nn.ConvTranspose1d(freq-1, freq-1, 3, stride=2, padding=1, output_padding=1)
+        self.up_t3 = nn.ConvTranspose1d(freq-1, freq-1, 3, stride=2, padding=1, output_padding=1)
 
-	self.activ = nn.LeakyReLU()
+        self.activ = nn.LeakyReLU()
 
         self.inp_size = input_size
         self.out_size = output_size
         self.lstm_1 = nn.GRU((freq-1)/4, (freq-1)/4, num_layers=1, batch_first=True, dropout=0.3)
         self.lstm_2 = nn.GRU(freq-1, freq-1, num_layers=1, batch_first=True, dropout=0.3)
-	self.dropout = nn.Dropout(0.05)
+        self.dropout = nn.Dropout(0.05)
         self.freq = freq
 
         self.norm_f = nn.InstanceNorm1d(freq, track_running_stats=False)
-	self.norm_0 = nn.InstanceNorm1d(freq-1, track_running_stats=False)
-	self.norm_1 = nn.InstanceNorm1d((freq-1)/2, track_running_stats=False)
-	self.norm_2 = nn.InstanceNorm1d((freq-1)/4, track_running_stats=False)
-	self.norm_3 = nn.InstanceNorm1d((freq-1)/8, track_running_stats=False)
+        self.norm_0 = nn.InstanceNorm1d(freq-1, track_running_stats=False)
+        self.norm_1 = nn.InstanceNorm1d((freq-1)/2, track_running_stats=False)
+        self.norm_2 = nn.InstanceNorm1d((freq-1)/4, track_running_stats=False)
+        self.norm_3 = nn.InstanceNorm1d((freq-1)/8, track_running_stats=False)
 
         self.res_block1 = res_block((freq-1)/1, 32)
         self.res_block2 = res_block((freq-1)/2, 32)
@@ -179,33 +179,32 @@ class net_in_v2(nn.Module):
         C1 = self.activ( self.down_f0(inp)  )
         C1 = self.activ( self.down_t0(C1)  )
         C2 = self.activ( self.down_f1(C1)  )
-	C3 = self.activ( self.down_t1(C2)  )
-	C4 = self.norm_2( self.down_f2(C3) )
+        C3 = self.activ( self.down_t1(C2)  )
+        C4 = self.norm_2( self.down_f2(C3) )
 
         C4, hidden = self.lstm_1(C4.permute(0,2,1))
-	C4 = C4.permute(0,2,1)
-	C5 = self.activ(  self.down_t2(C4)  )
-	encoding1 = self.activ(  self.down_f3(C5)  )
+        C4 = C4.permute(0,2,1)
+        C5 = self.activ(  self.down_t2(C4)  )
+        encoding1 = self.activ(  self.down_f3(C5)  )
 
         encoding = torch.cat((encoding1, pitch_code), 1)
 
         # Decode
-	C6 = self.activ( self.up_f1(encoding)   )
-	C5 = self.activ( self.up_t1(C6 + self.res_block3(C5))    )
-	C4 = self.activ( self.up_f2(C5)   )
-	C3 = self.activ( self.up_t2(C4 + self.res_block2(C3))  )
-	C2 = self.activ( self.norm_0( self.up_f3(C3)  ) )
-	output = self.norm_0( self.up_t3(C2 + self.res_block1(C1)) )
+        C6 = self.activ( self.up_f1(encoding)   )
+        C5 = self.activ( self.up_t1(C6 + self.res_block3(C5))    )
+        C4 = self.activ( self.up_f2(C5)   )
+        C3 = self.activ( self.up_t2(C4 + self.res_block2(C3))  )
+        C2 = self.activ( self.norm_0( self.up_f3(C3)  ) )
+        output = self.norm_0( self.up_t3(C2 + self.res_block1(C1)) )
 
-	#output = self.activ(  self.up_f4(output)   )
+        #output = self.activ(  self.up_f4(output)   )
 
-	output, hidden = self.lstm_2(output.permute(0,2,1))
+        output, hidden = self.lstm_2(output.permute(0,2,1))
 
-	output = self.activ(  self.up_f4(output.permute(0,2,1))   )
+        output = self.activ(  self.up_f4(output.permute(0,2,1))   )
         
         #return output
         return output, C2
-
 
 
 
